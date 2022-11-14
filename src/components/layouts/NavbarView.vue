c4ce7518d16a07f21a14acd56e88f20a73a96b04
hilman-firdd <hilmanfirdaus48@gmail.com>
2022-11-14 23:52:19 +0700

    #2 | component slice

---
+++ src/components/layouts/NavbarView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import Logo from "./LogoView.vue";
+import Navigation from "./NavigationView.vue";
+import UserInfo from "./UserInfoView.vue";
+</script>
+
+<template>
+  <nav
+    class="mx-auto max-w-7xl bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800"
+  >
+    <div
+      class="container flex flex-wrap items-center justify-between mx-auto my-2"
+    >
+      <logo/>
+      <Navigation/>
+      <UserInfo/>
+    </div>
+  </nav>
+</template>
