5c6b93f6125f95133fde5d88ce59f72218f4c284
hilman-firdd <hilmanfirdaus48@gmail.com>
2022-11-17 00:13:02 +0700

    #4 | tidy up component items 2

--- src/components/CategoriesCard.vue
+++ src/components/CategoriesCard.vue
@@ -1,4 +1,6 @@
 <script setup>
+    import { RouterLink } from 'vue-router';
+    
     defineProps({
         title: String,
         image: String,
@@ -9,11 +11,11 @@
 <template>
     <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
         <div class="overflow-hidden border border-gray-200 rounded-xl">
-            <router-link to="#">
+            <RouterLink to="#">
                 <div class="m-4 overflow-hidden rounded-xl">
                     <img :alt="title" class="block w-full h-auto" :src="'/src/assets/img/'+ image" />
                 </div>
-            </router-link>
+            </RouterLink>
 
             <header class="px-4 mb-4 leading-tight">
                 <h1 class="text-lg">
